51e3b77f056425fb177d6e1f40858c7d2278e095
Diener Dornelas <diener.ld@outlook.com>
2024-11-17 21:31:15 -0300

    feat: remove imagem e ajusta estilização para centralizar texto

--- app/modules/home/components/Presentation.vue
+++ app/modules/home/components/Presentation.vue
@@ -7,44 +7,47 @@ const { t } = useI18n({ useScope: 'local' })
 </script>
 
 <template>
-  <div class="mt-4 flex gap-2">
-    <div class="flex flex-col justify-around sm:items-center">
-      <ClientOnly>
+  <ClientOnly>
+    <div class="mt-4">
+      <div class="flex flex-col justify-around sm:items-center gap-8">
         <UiBlurReveal>
-          <h2 class="text-5xl w-full md:text-7xl text-center md:text-start">
+          <h2 class="text-5xl font-bold w-full md:text-7xl text-center">
             {{ t('title') }}
           </h2>
-          <h1 class="text-lg text-center mt-2 w-full md:text-start">
+          <h1 class="text-lg text-center w-full mt-6">
             {{ t('description') }}
           </h1>
         </UiBlurReveal>
-        <div class="w-full mt-8 flex items-center justify-center gap-4 md:justify-start lg:mt-0">
+
+        <div class="w-full mt-8 flex items-center justify-center gap-4  lg:mt-0">
           <UButton
             type="button"
             :label="t('buttons.cv')"
             :href="cv"
             target="_blank"
+            size="xl"
           />
           <UButton
             :label="t('buttons.contact')"
             type="button"
             :href="`mailto:${contact.email}`"
             target="_blank"
+            size="xl"
           />
         </div>
-      </ClientOnly>
+      </div>
+      <!-- <div class="hidden md:flex flex-col justify-center md:items-center">
+        <img
+          :src="img"
+          alt="Diener Dornelas"
+          class="w-full rounded-xl"
+          style="object-fit: cover;"
+          loading="lazy"
+          decoding="async"
+        >
+      </div> -->
     </div>
-    <div class="hidden md:flex flex-col justify-center md:items-center">
-      <img
-        :src="img"
-        alt="Diener Dornelas"
-        class="w-full rounded-xl"
-        style="object-fit: cover;"
-        loading="lazy"
-        decoding="async"
-      >
-    </div>
-  </div>
+  </ClientOnly>
 </template>
 
 <i18n lang="json">
